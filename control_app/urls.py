49d7bfc94d081b7ee0061e4a1c9cf8e4a994b1e0
Dias Bolatov <deobackstep@gmail.com>
2025-10-20 15:14:06 +0500

    urls

--- control_app/urls.py
+++ control_app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+    path('student_type', StudentTypeApiView.as_view()),
+    path('student', StudentApiView.as_view()),
+    path('lesson', LessonApiView.as_view()),
+    path('pack', PackApiView.as_view()),
+    path('payment', PaymentApiView.as_view()),
+    path('subject', SubjectApiView.as_view()),
 ]
